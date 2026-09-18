c08f93c642a6d4f11c505dbb62ead2c399a20294
SanujTiwari <sanujvirat@gmail.com>
2026-09-18 23:29:27 +0530

    Create IP Quest landing page and topics page

--- IPQuest/Controllers/HomeController.cs
+++ IPQuest/Controllers/HomeController.cs
@@ -1,25 +1,19 @@
-using IPQuest.Models;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
 
-namespace IPQuest.Controllers
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    public IActionResult Index()
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        return View();
+    }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+    public IActionResult Privacy()
+    {
+        return View();
+    }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+    public IActionResult Topics()
+    {
+        return View();
     }
 }
