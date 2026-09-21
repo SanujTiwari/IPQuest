66cc46f2da901423df25472ca0bb4c44325e971e
SanujTiwari <sanujvirat@gmail.com>
2026-09-21 12:16:17 +0530

    started the actual game functionality

---
+++ IPQuest/Models/AnswerOption.cs
@@ -0,0 +1,15 @@
+﻿namespace IPQuest.Models
+{
+    public class AnswerOption
+    {
+        public int Id { get; set; }
+
+        public string Text { get; set; } = string.Empty;
+
+        public bool IsCorrect { get; set; }
+
+        public int QuestionId { get; set; }
+
+        public Question Question { get; set; } = null!;
+    }
+}
---
+++ IPQuest/Models/Question.cs
@@ -0,0 +1,15 @@
+﻿namespace IPQuest.Models
+{
+    public class Question
+    {
+        public int Id { get; set; }
+
+        public string Text { get; set; } = string.Empty;
+
+        public int TopicId { get; set; }
+
+        public Topic Topic { get; set; } = null!;
+
+        public List<AnswerOption> AnswerOptions { get; set; } = new();
+    }
+}
